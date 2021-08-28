ebf1defe0186376cc9c5f5604c05d97f5c0238e0
JLaRoc182 <47267449+JLaRoc182@users.noreply.github.com>
2021-08-27 20:53:16 -0400

    Improved js and html fils

--- Calculator_v1.js
+++ Calculator_v1.js
@@ -16,7 +16,7 @@ function divide(x, y) {
 }
 
 function operate(operator, x, y){  /* Need to enter a string for operator '+', '-', '*', or '/' */
-    if(operator == '+') {
+    if (operator == '+') {
        return add(x, y);
     }
     else if (operator == '-') {
@@ -26,7 +26,13 @@ function operate(operator, x, y){  /* Need to enter a string for operator '+', '
    return multiply(x, y);
     }
     else if (operator == '/') {
-   return divide(x, y);
+        if (y == 0) { 
+        return "Hey not cool man, you can't divide by zero mannnnnnnn."
+        }
+        else {
+         return divide(x, y);
+
+        };
     }
     else {
         return "NOT AN OPERATOR";
@@ -45,21 +51,33 @@ const btn7 = document.querySelector('#btn7');
 const btn8 = document.querySelector('#btn8');
 const btn9 = document.querySelector('#btn9');
 const btn0 = document.querySelector('#btn0');
-const btnEquals = document.querySelector('#btnEquals');
-const btnClear = document.querySelector('#btnClear');
+const btnDecimal = document.querySelector('#btnDecimal');
 const btnAdd = document.querySelector('#btnAdd');
 const btnSubtract = document.querySelector('#btnSubtract');
 const btnMultiply = document.querySelector('#btnMultiply');
 const btnDivide = document.querySelector('#btnDivide');
+const btnEquals = document.querySelector('#btnEquals');
+const btnClear = document.querySelector('#btnClear');
+const btnDelete = document.querySelector('#btnDelete');
 
+
+document.addEventListener('keydown', (event) => {
+    var name = event.key;
+    var code = event.code;
+    // Alert the key name and key code on keydown
+    alert(`Key pressed ${name} \r\n Key code value: ${code}`);
+  }, false);
 
 
 let myArray1 = [];
 let myStr1 = '';
 let myNum1 = 0;
-let myMem1 = 0;
-let myOperator = '';
+let myMem1 = 0;  
+let myOperator = '+';
 let myResult = 0;
+let lastButton = 'Clear';
+
+
 
 function clearDisplay() {
     let oldDiv = document.getElementById('resultDisplay');
@@ -76,24 +94,63 @@ function setDisplay(display){
 
 function pushDigit(button) {
     button.addEventListener("click", () => {
+        if (lastButton !== 'Equals') {
+            clearDisplay();
+            myArray1.push(button.textContent);
+            myStr1 = myArray1.join('');
+            myNum1 = Number(myStr1);
+            setDisplay(myNum1);
+            lastButton = 'Digit';
+        };
+    });
+};
+
+function pushDecimal(button) {
+    button.addEventListener("click", () => {
+        if (lastButton =='Digit') {
+            clearDisplay();
+            myArray1.push(button.textContent);
+            myStr1 = myArray1.join('');
+            myNum1 = Number(myStr1);
+            setDisplay(myNum1 + '.');
+            lastButton = 'Decimal';
+        }
+        else if ((lastButton =='Clear') || (lastButton == 'Operator')) {
+            clearDisplay();
+            myArray1.push('0');
+            myArray1.push(button.textContent);
+            myStr1 = myArray1.join('');
+            myNum1 = Number(myStr1);
+            setDisplay(myNum1 + '.');
+            lastButton = 'Decimal';
+        }
+    });
+};
+
+function pushDelete(button) {
+    button.addEventListener("click", () => {
+        if (lastButton !== 'Equals') {
         clearDisplay();
-        myArray1.push(button.textContent);
-        let myStr1 = myArray1.join('');
-        myNum1 = parseInt(myStr1);
-        setDisplay(myNum1);
+        myArray1.pop();
+        myStr1 = myArray1.join('');
+        myNum1 = Number(myStr1);
+        setDisplay(myNum1); 
+        };
     });
 };
 
 function pushOperator(button) {
-    button.addEventListener("click", () => {
-        clearDisplay();
-        setDisplay(button.textContent);
-        myOperator = button.textContent;
-        myMem1 = myNum1;
-        myArray1 = [];
-        myStr1 = '';
-        myNum1 = 0;
-
+    button.addEventListener("click", () => {         
+        if ((lastButton == 'Digit') || (lastButton == 'Decimal') || (lastButton == 'Equals'))  {
+            myMem1 = operate(myOperator, myMem1, myNum1);
+            myOperator = button.textContent;
+            myArray1 = [];
+            myStr1 = '';
+            myNum1 = 0;
+            clearDisplay();
+            setDisplay(myMem1);
+            lastButton = 'Operator';
+        }
     });
 };
 
@@ -102,24 +159,25 @@ function pushEquals(button) {
         clearDisplay();
         myResult = operate(myOperator, myMem1, myNum1);
         setDisplay(myResult);
-        myOperator = '';
-        myMem1 = 0;
+        myOperator = '+';
         myArray1 = [];
         myStr1 = '';
-        myNum1 = myResult;
-        
+        myNum1 = 0;
+        myMem1 = myResult;
+        lastButton = 'Equals';
     });
 };
 
 function pushClear(button) {
     button.addEventListener("click", () => {
         clearDisplay();
-        myOperator = '';
+        myOperator = '+';
         myMem1 = 0;
         myArray1 = [];
         myStr1 = '';
         myNum1 = 0;
         myResult = 0;
+        lastButton = 'Clear';
     });
 };
 
@@ -135,9 +193,11 @@ pushDigit(btn7);
 pushDigit(btn8);
 pushDigit(btn9);
 pushDigit(btn0);
+pushDecimal(btnDecimal);
 pushOperator(btnAdd);
 pushOperator(btnSubtract);
 pushOperator(btnMultiply);
 pushOperator(btnDivide);
 pushEquals(btnEquals);
 pushClear(btnClear);
+pushDelete(btnDelete);
